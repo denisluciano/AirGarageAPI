ff98b0ac3c3dc6fd4cfec31ce1647611af4ad7dc
Denis Luciano <denis.lopes3012@gmail.com>
2020-05-12 11:47:46 -0300

    alterações para ir para ubuntu

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,17 @@
 'use strict'
 
 class SessionController {
-  async create ({ request, auth }) {
-    const { email, password } = request.all()
 
-    const token = await auth.attempt(email, password)
+  async create ({ request, auth, response }) {
 
-    return token
+    const { email, password } = request.all();
+
+    try {
+      const token = await auth.attempt(email, password)
+      response.send(token)
+    } catch (error) {
+      response.status(401).send()
+    }
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,4 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.get('/testapi', 'SessionController.test')
